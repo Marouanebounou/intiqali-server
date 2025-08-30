70c7f46fccce074e9ac4a43d13aa8643675c4b7d
marouane bounou <maroinbounou.123@gmail.com>
2025-08-30 22:42:15 +0100

    p

--- server.js
+++ server.js
@@ -30,16 +30,7 @@ const initializeApp = async () => {
     ].filter(Boolean);
 
     app.use(cors({
-        origin: function (origin, callback) {
-            // Allow requests with no origin (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
-            
-            if (allowedOrigins.indexOf(origin) !== -1) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
+        origin: allowedOrigins,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true
     }));
